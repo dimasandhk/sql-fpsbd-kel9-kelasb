95ef6fd54974619f158400af1a2a195cff8635d9
dimasandhk <dimasandhikadiputra@gmail.com>
2024-06-12 23:11:51 +0700

    update: detail_game implement printInt func

--- view-query.sql
+++ view-query.sql
@@ -1,9 +1,23 @@
 CREATE VIEW detail_game AS
 SELECT  
-	game.title, genre.genre_name, developer.dev_name, game.release_date, game.ovr_reviews, game.price, game.description, game.age_rest,
-    system_requirements.min_os, system_requirements.recom_os, system_requirements.min_processor, system_requirements.recom_processor,
-    system_requirements.min_memory, system_requirements.recom_memory, system_requirements.min_graphics, system_requirements.recom_graphics,
-    system_requirements.min_storage, system_requirements.recom_storage
+    game.title, 
+    genre.genre_name, 
+    developer.dev_name, 
+    game.release_date, 
+    game.ovr_reviews, 
+    printInt(CAST(game.price AS INT), 'price') AS formatted_price, 
+    game.description, 
+    game.age_rest,
+    system_requirements.min_os, 
+    system_requirements.recom_os, 
+    system_requirements.min_processor, 
+    system_requirements.recom_processor,
+    printInt(system_requirements.min_memory, 'memory') AS formatted_min_memory, 
+    printInt(system_requirements.recom_memory, 'memory') AS formatted_recom_memory, 
+    system_requirements.min_graphics, 
+    system_requirements.recom_graphics,
+    printInt(system_requirements.min_storage, 'storage') AS formatted_min_storage, 
+    printInt(system_requirements.recom_storage, 'storage') AS formatted_recom_storage
 FROM game 
 JOIN developer ON developer.dev_id = game.dev_id 
 JOIN system_requirements ON system_requirements.sysreq_id = game.sysreq_id
