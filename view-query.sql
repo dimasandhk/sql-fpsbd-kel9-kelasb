735c21e5580fa4f17fb72737f7447c9e02092209
HeavenlySpectre <144968322+HeavenlySpectre@users.noreply.github.com>
2024-06-13 08:19:50 +0700

    Update view-query.sql

--- view-query.sql
+++ view-query.sql
@@ -24,3 +24,44 @@ JOIN system_requirements ON system_requirements.sysreq_id = game.sysreq_id
 JOIN game_genres ON game_genres.game_id = game.game_id
 JOIN genre ON genre.genre_id = game_genres.genre_id;
 
+----------------------------------------------------------------------------------------------
+
+CREATE VIEW `adult_rating` AS
+SELECT 
+    g.title AS game_title,
+    d.dev_name AS developer_name,
+    g.release_date,
+    g.ovr_reviews,
+    g.price,
+    g.description,
+    sr.min_os,
+    sr.min_processor,
+    printInt(sr.min_memory, 'memory') AS formatted_min_memory, 
+    sr.min_graphics,
+    printInt(sr.min_storage, 'storage') AS formatted_min_storage,
+    GROUP_CONCAT(DISTINCT gen.genre_name ORDER BY gen.genre_name ASC SEPARATOR ', ') AS genres
+FROM 
+    game g
+JOIN 
+    developer d ON g.dev_id = d.dev_id
+JOIN 
+    system_requirements sr ON g.sysreq_id = sr.sysreq_id
+JOIN 
+    game_genres gg ON g.game_id = gg.game_id
+JOIN 
+    genre gen ON gg.genre_id = gen.genre_id
+WHERE 
+    g.age_rest = 18
+GROUP BY 
+    g.game_id, 
+    g.title, 
+    d.dev_name, 
+    g.release_date, 
+    g.ovr_reviews, 
+    g.price, 
+    g.description, 
+    sr.min_os, 
+    sr.min_processor, 
+    sr.min_memory, 
+    sr.min_graphics, 
+    sr.min_storage;
